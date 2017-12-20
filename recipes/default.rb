0c33d9d01eab6993292dfeff05d56d47b3bed5c7
dvita <heygirl@davitastrong.com>
2017-12-20 11:15:20 -0800

    Version 1.0.0 updated default.rb to include search

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,16 +4,20 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-node.default['haproxy']['members'] = [{
-	'hostname' => 'ec2-34-201-65-78.compute-1.amazonaws.com',
-	'ipaddress' => '34.201.65.78',
-	'port' => 80,
-	'ssl_port' => 80
-},{
-	'hostname' => 'ec2-52-90-184-212.compute-1.amazonaws.com',
-	'ipaddress' => '52.90.184.212',
-	'port' => 80,
-	'ssl_port' => 80
-}]
+all_web_nodes = search('node','role:web_server')
+
+members = []
+
+all_web_nodes.each do |web_node|
+	member = {
+		'hostname' => web_node['cloud']['public_hostname'],
+		'ipaddress' => web_node['cloud']['public_ipv4'],
+		'port' => 80,
+		'ssl_port' => 80
+	}
+	members.push(member)
+end
+
+node.default['haproxy']['members'] = members
 
 include_recipe 'haproxy::manual'
