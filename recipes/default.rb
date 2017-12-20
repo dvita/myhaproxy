991cc52a7ea020f07d2875fe3918df97453df3da
dvita <heygirl@davitastrong.com>
2017-12-19 18:38:47 -0800

    initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+node.default['haproxy']['members'] = [{
+	'hostname' => 'ec2-34-201-65-78.compute-1.amazonaws.com',
+	'ipaddress' => '34.201.65.78',
+	'port' => 80,
+	'ssl_port' => 80
+},{
+	'hostname' => 'ec2-52-90-184-212.compute-1.amazonaws.com',
+	'ipaddress' => '52.90.184.212',
+	'port' => 80,
+	'ssl_port' => 80
+}]
+
+include_recipe 'haproxy::manual'
